1be37e60d74b68c43cd0434c5c6c49dc3dbe491e
Kassie Wang <klw242@cornell.edu>
2021-10-20 20:45:25 -0400

    Restart new react app

---
+++ pe-app/src/App.js
@@ -0,0 +1,19 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import "./App.css";
+import Home from "components/Home.js";
+
+function App() {
+	return (
+		<BrowserRouter>
+			<div>
+				<Navigation />
+				<Switch>
+					<Route exact path="/" component={Home} />
+					<Route path="/about" component={About} />
+				</Switch>
+			</div>
+		</BrowserRouter>
+	);
+}
+
+export default App;
