93e614413b7739f604b63eb5cb0bb8a51483ee01
Jiwoo Cheon <jc2762@cornell.edu>
2021-12-20 13:32:52 -0500

    merged in artist filter, needs to be connected to data

--- pe-app/client/src/components/ArtPage.js
+++ pe-app/client/src/components/ArtPage.js
@@ -2,10 +2,18 @@ import exportedData from "../util/records_new.json";
 import React, { useState } from "react";
 import { Checkbox, FormGroup, FormControlLabel, Grid } from "@material-ui/core";
 import EssayCard from "./EssayCard.js";
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
+import CheckBoxIcon from '@mui/icons-material/CheckBox';
 import ArtworkCard from "./ArtworkCard.js";
 import PoetryCard from "./PoetryCard.js";
 import Modal from "./Modal.js";
 import Navbar from "./Navbar.js";
+
+const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
+const checkedIcon = <CheckBoxIcon fontSize="small" />;
+const authors = [{ label: 'author1' }, { label: 'author2' }];
 
 export default function ArtPage() {
 	const [show, setShow] = useState(false);
@@ -21,6 +29,28 @@ export default function ArtPage() {
 		<div>
 			<Navbar />
 			<FormGroup style={{ display: "inline-block" }}>
+				<FormControlLabel control={
+					<Autocomplete
+						multiple
+						limitTags={2}
+						options={authors}
+						getOptionLabel={(option) => option.label}
+						sx={{ width: 250 }}
+						renderOption={(props, option, { selected }) => (
+							<li {...props}>
+								<Checkbox
+									icon={icon}
+									checkedIcon={checkedIcon}
+									style={{ marginRight: 8, color: "#DD9933" }}
+									checked={selected}
+									defaultChecked
+								/>
+								{option.label}
+							</li>
+						)}
+						renderInput={(params) => <TextField {...params} label="Selected Artists" />} />
+				}
+				/>
 				<FormControlLabel
 					control={
 						<Checkbox defaultChecked style={{ color: "#DD9933" }} />
