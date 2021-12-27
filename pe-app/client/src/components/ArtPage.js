a4bfd21d4bef926420db0a6b6d4d2c888714ece6
Kassie Wang <klw242@cornell.edu>
2021-12-26 23:29:37 -0500

    Start fixing font and other small styling

--- pe-app/client/src/components/ArtPage.js
+++ pe-app/client/src/components/ArtPage.js
@@ -1,10 +1,10 @@
 import exportedData from "../util/records_new.json";
 import React, { useState } from "react";
 import { Checkbox, FormGroup, FormControlLabel, Grid } from "@material-ui/core";
-import TextField from '@mui/material/TextField';
-import Autocomplete from '@mui/material/Autocomplete';
-import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
-import CheckBoxIcon from '@mui/icons-material/CheckBox';
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
+import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
+import CheckBoxIcon from "@mui/icons-material/CheckBox";
 import ArtworkCard from "./ArtworkCard.js";
 import Navbar from "./Navbar.js";
 
@@ -13,9 +13,10 @@ const checkedIcon = <CheckBoxIcon fontSize="small" />;
 const data = exportedData.filter(
 	(entry) => entry["Program (category)"] == "Art"
 );
-const authors = data.map((entry) =>
-	(entry["Author Name"] + " " + entry["Last Name"]));
-const uniqueAuthors = [... new Set(authors)].map((entry) => ({ label: entry }));
+const authors = data.map(
+	(entry) => entry["Author Name"] + " " + entry["Last Name"]
+);
+const uniqueAuthors = [...new Set(authors)].map((entry) => ({ label: entry }));
 
 export default function ArtPage() {
 	const [show, setShow] = useState(false);
@@ -24,27 +25,37 @@ export default function ArtPage() {
 		<div>
 			<Navbar />
 			<FormGroup style={{ display: "inline-block" }}>
-				<FormControlLabel control={
-					<Autocomplete
-						multiple
-						limitTags={2}
-						options={uniqueAuthors}
-						getOptionLabel={(option) => option.label}
-						sx={{ width: 250 }}
-						renderOption={(props, option, { selected }) => (
-							<li {...props}>
-								<Checkbox
-									icon={icon}
-									checkedIcon={checkedIcon}
-									style={{ marginRight: 8, color: "#DD9933" }}
-									checked={selected}
-									defaultChecked
+				<FormControlLabel
+					control={
+						<Autocomplete
+							multiple
+							limitTags={2}
+							options={uniqueAuthors}
+							getOptionLabel={(option) => option.label}
+							sx={{ width: 250 }}
+							renderOption={(props, option, { selected }) => (
+								<li {...props}>
+									<Checkbox
+										icon={icon}
+										checkedIcon={checkedIcon}
+										style={{
+											marginRight: 8,
+											color: "#DD9933",
+										}}
+										checked={selected}
+										defaultChecked
+									/>
+									{option.label}
+								</li>
+							)}
+							renderInput={(params) => (
+								<TextField
+									{...params}
+									label="Selected Artists"
 								/>
-								{option.label}
-							</li>
-						)}
-						renderInput={(params) => <TextField {...params} label="Selected Artists" />} />
-				}
+							)}
+						/>
+					}
 				/>
 				<FormControlLabel
 					control={
--- pe-app/client/src/components/JournalCard.js
+++ pe-app/client/src/components/JournalCard.js
@@ -4,6 +4,8 @@ import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import CardActions from "@material-ui/core/CardActions";
+import { makeStyles } from "@mui/styles";
+import "../css/CardStyling.css";
 
 const dates = [
 	"January",
@@ -31,13 +33,6 @@ export default function JournalCard(cardData) {
 					]
 				}
 				alt="prisoner art"
-				style={{
-					width: "84px",
-					height: "115px",
-					float: "right",
-					marginRight: "14px",
-					marginTop: "15.5px",
-				}}
 			/>
 		);
 	} else {
@@ -52,98 +47,84 @@ export default function JournalCard(cardData) {
 	}
 
 	return (
-		<div style={{}}>
-			<h4></h4>
-			{
-				<Card
-					style={{
-						boxShadow: "0px 0px 10px 5px rgba(0, 0, 0, 0.1)",
-						borderRadius: "20px",
-						width: "314px",
-						height: "146px",
-					}}
-				>
-					<CardContent
+		<div>
+			<Card>
+				<CardContent>
+					{image}
+					<Typography
 						style={{
-							padding: "0px",
+							fontFamily: "Open Sans",
+							fontStyle: "normal",
+							fontWeight: "bold",
+							fontSize: "20px",
+							paddingTop: "16px",
+							paddingLeft: "16px",
+						}}
+						color="#000000"
+					>
+						{cardData.cardData["Author Name"] +
+							" " +
+							cardData.cardData["Last Name"]}
+					</Typography>
+
+					<Typography
+						variant="h5"
+						component="h2"
+						style={{
+							fontFamily: "Open Sans",
+							fontStyle: "normal",
+							fontWeight: "normal",
+							fontSize: "14px",
+							paddingTop: "3.83px",
+							paddingLeft: "16px",
+						}}
+						color="black"
+					>
+						{cardData.cardData["City"] +
+							", " +
+							cardData.cardData["State"]}
+					</Typography>
+
+					<Typography
+						style={{
+							marginBottom: 12,
+							fontFamily: "Open Sans",
+							fontWeight: 400,
+							fontStyle: "normal",
+							fontSize: "11.35px",
+							paddingTop: "5px",
+							paddingLeft: "16px",
+							color: "#828282",
 						}}
 					>
-						{image}
-						<Typography
-							style={{
-								fontFamily: "Open Sans",
-								fontStyle: "normal",
-								fontWeight: "bold",
-								fontSize: "20px",
-								paddingTop: "16px",
-								paddingLeft: "16px",
-							}}
-							color="#000000"
-						>
-							{cardData.cardData["Author Name"] +
-								" " +
-								cardData.cardData["Last Name"]}
-						</Typography>
-
-						<Typography
-							variant="h5"
-							component="h2"
-							style={{
-								fontFamily: "Open Sans",
-								fontStyle: "normal",
-								fontWeight: "normal",
-								fontSize: "14px",
-								paddingTop: "3.83px",
-								paddingLeft: "16px",
-							}}
-							color="black"
-						>
-							{cardData.cardData["City"] +
-								", " +
-								cardData.cardData["State"]}
-						</Typography>
-
-						<Typography
-							style={{
-								marginBottom: 12,
-								fontFamily: "Open Sans",
-								fontWeight: 400,
-								fontStyle: "normal",
-								fontSize: "11.35px",
-								paddingTop: "5px",
-								paddingLeft: "16px",
-								color: "#828282",
-							}}
-						>
-							{dates[
-								parseInt(
-									cardData.cardData[
-										"Last modified time"
-									].split("-")[1]
-								) - 1
-							] +
-								" " +
+						{dates[
+							parseInt(
 								cardData.cardData["Last modified time"].split(
 									"-"
-								)[0]}
-						</Typography>
+								)[1]
+							) - 1
+						] +
+							" " +
+							cardData.cardData["Last modified time"].split(
+								"-"
+							)[0]}
+					</Typography>
 
-						<Typography
-							style={{
-								fontFamily: "Open Sans",
-								fontStyle: "normal",
-								fontWeight: "normal",
-								fontSize: "11.3494px",
-								paddingTop: "20px",
-								paddingLeft: "16px",
-								color: "#828282",
-							}}
-						>
-							{responses + " Responses"}
-						</Typography>
-					</CardContent>
-				</Card>
-			}
+					<Typography
+						style={{
+							fontFamily: "Open Sans",
+							fontStyle: "normal",
+							fontWeight: "normal",
+							fontSize: "11.3494px",
+							paddingTop: "20px",
+							paddingLeft: "16px",
+							color: "#828282",
+						}}
+					>
+						{responses + " Responses"}
+					</Typography>
+				</CardContent>
+			</Card>
 		</div>
 	);
 }
