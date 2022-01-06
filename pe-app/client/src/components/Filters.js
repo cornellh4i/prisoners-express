4d90929a3a620d41ef453b1bd0ae01494b661d9a
Jiwoo Cheon <jc2762@cornell.edu>
2022-01-06 01:01:07 -0500

    fixed filter not populating, reset button, and filter size

--- pe-app/client/src/components/Filters.js
+++ pe-app/client/src/components/Filters.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { Checkbox, FormGroup, FormControlLabel, Button } from "@material-ui/core";
+import React, { useEffect, useState } from "react";
+import { Chip, Checkbox, FormGroup, FormControlLabel, Button } from "@material-ui/core";
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
 import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
@@ -9,9 +9,6 @@ import { makeStyles } from "@material-ui/core/styles";
 const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
 const checkedIcon = <CheckBoxIcon fontSize="small" />;
 const useStyles = makeStyles({
-  checkbox: {
-    marginRight: 8, color: "#DD9933"
-  },
   resetButton: {
     color: "black",
     border: "solid gray 1px",
@@ -41,12 +38,23 @@ export default function Filters(props) {
     setResponses(!showResponses);
   }
 
+  console.log('unique data', uniqueData);
+  const [defaultAuthors, setDefaultAuthors] = useState(uniqueData);
+  const [selectedAuthors, setSelectedAuthors] = useState(uniqueData);
+  const [authorsScroll, setAuthorsScroll] = useState(false);
+
+  useEffect(() => {
+    setDefaultAuthors(uniqueData)
+    setSelectedAuthors(uniqueData)
+  }, [uniqueData]);
+
+
   const resetFilters = (event) => {
     setNoResponse(true);
     setResponses(true);
     setAuthors(uniqueData);
+    setSelectedAuthors(uniqueData)
   }
-
 
   return (
     <FormGroup style={{ display: "inline-block", paddingLeft: '3%' }}>
@@ -58,21 +66,55 @@ export default function Filters(props) {
           getOptionLabel={(option) => option}
           sx={{ width: 250 }}
           size="small"
-          defaultValue={uniqueData}
-          onChange={(event, newValue) => {
+          onChange={(event, newValue, reason) => {
             setAuthors(newValue);
+            setSelectedAuthors(newValue);
           }}
+          value={selectedAuthors}
+          renderTags={(value, getTagProps) => {
+            const numTags = value.length;
+            if (authorsScroll) {
+              return (
+                <div className="d-flex" style={{ maxHeight: '20vh', overflow: 'auto' }}>
+                  {value.map((option, index) => (
+                    <Chip
+                      color="gray"
+                      label={option}
+                      size="small"
+                      {...getTagProps({ index })}
+                    />
+                  ))}
+                </div>
+              );
+            } else {
+              return (
+                value.map((option, index) => (
+                  <Chip
+                    color="gray"
+                    label={option}
+                    size="small"
+                    {...getTagProps({ index })}
+                  />
+                )));
+            }
+          }}
+          onFocus={() => setAuthorsScroll(true)}
+          onBlur={() => setAuthorsScroll(false)}
           renderOption={(props, option, { selected }) => (
-            <li {...props}>
-              <Checkbox
-                icon={icon}
-                checkedIcon={checkedIcon}
-                className={classes.checkbox}
-                checked={selected}
-              />
-              {option}
-            </li>
+            <div style={{ maxHeight: '100px', overFlowY: "auto" }}>
+              <li {...props} >
+                <Checkbox
+                  icon={icon}
+                  checkedIcon={checkedIcon}
+                  className={classes.checkbox}
+                  checked={selected}
+                  style={{ color: "#DD9933" }}
+                />
+                {option}
+              </li>
+            </div>
           )}
+
           renderInput={(params) => <TextField {...params}
             label={category == "Art" ? "Selected Artists" : "Selected Authors"} />} />
       }
