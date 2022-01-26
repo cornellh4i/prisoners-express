30ba70dab42cfb47f94674bb1a4519c718f1139b
Kassie Wang <klw242@cornell.edu>
2022-01-25 23:34:02 -0500

    Final build hopefully

--- pe-app/api/src/index.js
+++ pe-app/api/src/index.js
@@ -5,11 +5,15 @@ const cors = require("cors");
 const port = process.env.PORT || 5000;
 const { setIntervalAsync } = require("set-interval-async/dynamic");
 const app = express();
+const path = require("path");
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cors({ origin: /http:\/\/(127(\.\d){3}|localhost)/ }));
+app.use(cors({ origin: "*" }));
 app.options("*", cors());
 require("dotenv").config();
+
+app.use(express.static(path.join(__dirname, "../client/build")));
 
 const url = `https://api.airtable.com/v0/${process.env.BASE_ID}/${process.env.TABLE_NAME}`;
 
@@ -18,7 +22,12 @@ cache.triggerUpdate();
 setIntervalAsync(cache.triggerUpdate, 5 * 60000); // refresh every 5 min
 
 app.get("/", function (req, res) {
+	// console.log(cache.data);
 	res.send(cache.data);
+});
+
+app.get("*", (req, res) => {
+	res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
 });
 
 app.listen(port, function () {
