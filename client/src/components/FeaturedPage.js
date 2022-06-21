a3f9bc23375efcc17d566c1e48c4a043faac02ac
Zaeda Amrin <za48@cornell.edu>
2022-06-21 12:54:02 -0400

    Console log data (temp)

--- client/src/components/FeaturedPage.js
+++ client/src/components/FeaturedPage.js
@@ -63,7 +63,7 @@ export default function FeaturedPage() {
       .then((response) => response.json())
       .then((d) => d.filter((entry) => entry["Display"] === true))
       .then((d) => {
-        // console.log(d);
+        console.log(d);
         setData(d);
       });
   }, []);
